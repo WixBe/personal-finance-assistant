a38131d1f9fd559da5efd15b05a9d1932c692291
Narendran M <narendranmurali2001@gmail.com>
2024-09-15 23:30:26 +0530

    features tab

--- frontend/finance-assistant/src/app/app.routes.ts
+++ frontend/finance-assistant/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { FeaturesComponent } from './features/features.component';
 
 export const routes: Routes = [
     { path:'', redirectTo: 'home', pathMatch: 'full' },
     { path:'home', component: HomeComponent },
+    { path:'features', component: FeaturesComponent },
 ];
