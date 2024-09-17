4b0f48bb1f6c830af0dd2fe1c8ce3ed32db6b251
Narendran M <narendranmurali2001@gmail.com>
2024-09-17 21:53:54 +0530

    vanta bg fix

--- frontend/finance-assistant/src/app/home/home.component.ts
+++ frontend/finance-assistant/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component} from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID} from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -8,6 +9,53 @@ import { RouterLink } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+
+export class HomeComponent implements OnInit{
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
+  
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.loadVantaGlobe();
+    }
+  }
+
+  loadVantaGlobe(): void {
+    if (!document.getElementById('vanta-globe-script')) {
+      const script = document.createElement('script');
+      script.id = 'vanta-globe-script';
+      script.src = 'https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.globe.min.js';
+      script.onload = () => {
+        this.initializeVanta();
+      };
+      document.body.appendChild(script);
+    } else {
+      this.initializeVanta();
+    }
+  }
+
+  initializeVanta(): void {
+    if (window['VANTA']) {
+      window['VANTA'].GLOBE({
+        el: "#vanta-bg-globe",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        color: 0x3f60ff,
+        size: 3,
+        backgroundColor: 0xffffff
+      });
+      }
+  }
 }
-// 
+
+declare global {
+  interface Window {
+    VANTA: any;
+  }
+}
